6cdcc1614daceac7b7b14daa0d184e2813aebcb4
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-09-01 09:06:23 +0100

    Manually select ISPyB credentials json file

--- src/relion/cli/create_cluster_monitor_table.py
+++ src/relion/cli/create_cluster_monitor_table.py
@@ -1,9 +1,20 @@
+import argparse
+import json
+import pathlib
+
 import sqlalchemy
 
 from relion.zocalo import alchemy
 
 
 def run():
-    url = alchemy.buffer_url()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--credentials", dest="creds")
+    args = parser.parse_args()
+    configuration = pathlib.Path(args.creds).read_text()
+    secret_ingredients = json.loads(configuration)
+    url = "mysql+mysqlconnector://{user}:{passwd}@{host}:{port}/{db}".format(
+        **secret_ingredients
+    )
     engine = sqlalchemy.create_engine(url, echo=True, connect_args={"use_pure": True})
     alchemy.ClusterMonitoring.__table__.create(engine)
