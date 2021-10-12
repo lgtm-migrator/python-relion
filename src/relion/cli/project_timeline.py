4950a2ca458c92f05aadd6d86d5ef2adebb4edf8
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-10-12 17:50:17 +0100

    colour based on schedule in time bar chart

--- src/relion/cli/project_timeline.py
+++ src/relion/cli/project_timeline.py
@@ -4,8 +4,6 @@ from datetime import datetime
 
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 from relion import Project
 
@@ -18,8 +16,20 @@ def run() -> None:
     relion_dir = pathlib.Path(args.proj_path)
     proj = Project(relion_dir, cluster=True)
 
-    preproc_job_times = {"start_time": [], "end_time": [], "job": []}
-    other_job_times = {"start_time": [], "end_time": [], "job": []}
+    preproc_job_times = {
+        "start_time": [],
+        "end_time": [],
+        "job": [],
+        "schedule": [],
+        "cluster_id": [],
+    }
+    other_job_times = {
+        "start_time": [],
+        "end_time": [],
+        "job": [],
+        "schedule": [],
+        "cluster_id": [],
+    }
     for job in proj._job_nodes.nodes:
         if "External" in job.name:
             tag = job.environment["alias"].split("/")[1]
@@ -36,11 +46,24 @@ def run() -> None:
                 preproc_job_times["job"].extend(
                     [tag.split("/")[0] for _ in job.environment["job_start_times"]]
                 )
+                preproc_job_times["schedule"].extend(
+                    ["preprocess" for _ in job.environment["job_start_times"]]
+                )
+                preproc_job_times["cluster_id"].extend(
+                    job.environment["cluster_job_ids"]
+                )
         else:
             tag = tag.split("_batch")[0]
             other_job_times["start_time"].append(job.environment["start_time_stamp"])
             other_job_times["end_time"].append(job.environment["end_time_stamp"])
             other_job_times["job"].append(tag.split("/")[0])
+            other_job_times["schedule"].append(tag)
+            if job.environment["cluster_job_ids"]:
+                other_job_times["cluster_id"].append(
+                    job.environment["cluster_job_ids"][0]
+                )
+            else:
+                other_job_times["cluster_id"].append(None)
     sorted_times = sorted(preproc_job_times["start_time"])
     drop_index = preproc_job_times["start_time"].index(sorted_times[-1])
     end_times = {ts: sorted_times[i + 1] for i, ts in enumerate(sorted_times[:-1])}
@@ -63,6 +86,7 @@ def run() -> None:
 
     df = pd.DataFrame(preproc_job_times)
     df_other = pd.DataFrame(other_job_times)
+    df_all = pd.concat([df, df_other])
     timeline = px.timeline(
         df,
         x_start="start_time",
@@ -70,48 +94,25 @@ def run() -> None:
         hover_name="job",
         color="job",
     )
-    class2d_trace = px.timeline(
-        df_other,
+    full_timeline = px.timeline(
+        df_all,
         x_start="start_time",
         x_end="end_time",
+        y="job",
         hover_name="job",
         color="job",
     )
-    fig = make_subplots(rows=2, cols=1, colshared_xaxes=True)
-    fig.add_trace(
-        go.timeline(
-            df,
-            x_start="start_time",
-            x_end="end_time",
-            hover_name="job",
-            color="job",
-        ),
-        row=1,
-        col=1,
-    )
-    fig.add_trace(
-        go.timeline(
-            df_other,
-            x_start="start_time",
-            x_end="end_time",
-            hover_name="job",
-            color="job",
-        ),
-        row=2,
-        col=1,
-    )
-    fig.write_html(pathlib.Path(args.out_dir) / "relion_project_timeline.html")
+
     timeline.write_html(
         pathlib.Path(args.out_dir) / "relion_project_preprocessing_timeline.html"
     )
-    class2d_trace.write_html(
-        pathlib.Path(args.out_dir) / "relion_project_classification_timeline.html"
+    full_timeline.write_html(
+        pathlib.Path(args.out_dir) / "relion_project_timeline.html"
     )
 
-    df_all = pd.concat([df, df_other])
     df_all.sort_values("start_time")
 
-    cumulative_time = px.bar(df_all, x="job", y="total_time")
+    cumulative_time = px.bar(df_all, x="job", y="total_time", color="schedule")
     cumulative_time.write_html(
         pathlib.Path(args.out_dir) / "cumulative_preprcoessing_job_time.html"
     )
