b5c59cc56e2ed49fcb6a553bdb23f3429d6813c6
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-10-12 15:06:24 +0100

    start to use dataframes for project_timeline

--- src/relion/cli/project_timeline.py
+++ src/relion/cli/project_timeline.py
@@ -15,10 +15,9 @@ def run() -> None:
     args = parser.parse_args()
     relion_dir = pathlib.Path(args.proj_path)
     proj = Project(relion_dir, cluster=True)
-    # mc_jobs = proj._job_nodes.nodes[1].environment["cluster_job_ids"]
 
-    preproc_job_times = []
-    other_job_times = []
+    preproc_job_times = {"start_time": [], "end_time": [], "job": []}
+    other_job_times = {"start_time": [], "end_time": [], "job": []}
     for job in proj._job_nodes.nodes:
         if "External" in job.name:
             tag = job.environment["alias"].split("/")[1]
@@ -29,41 +28,46 @@ def run() -> None:
                 job.environment["alias"] is None
                 or "Icebreaker_group_batch" not in job.environment["alias"]
             ):
-                preproc_job_times.extend(
-                    [(t, tag) for t in job.environment["job_start_times"]]
+                preproc_job_times["start_time"].extend(
+                    job.environment["job_start_times"]
+                )
+                preproc_job_times["job"].extend(
+                    [tag.split("/")[0] for _ in job.environment["job_start_times"]]
                 )
         else:
-            other_job_times.append(
-                (
-                    job.environment["start_time_stamp"],
-                    job.environment["end_time_stamp"],
-                    tag,
-                )
-            )
-    preproc_job_times = sorted(preproc_job_times, key=lambda x: x[0])
+            other_job_times["start_time"].append(job.environment["start_time_stamp"])
+            other_job_times["end_time"].append(job.environment["end_time_stamp"])
+            other_job_times["job"].append(tag.split("/")[0])
+    sorted_times = sorted(preproc_job_times["start_time"])
+    end_times = {ts: sorted_times[i + 1] for ts, i in enumerate(sorted_times[:-1])}
+    preproc_job_times["start_time"] = preproc_job_times["start_time"][:-1]
+    preproc_job_times["job"] = preproc_job_times["job"][:-1]
+    preproc_job_times["end_time"] = [
+        end_times[t] for t in preproc_job_times["start_time"]
+    ]
     class2d_job_times = [p for p in other_job_times if "Class2D" in p[2]]
     class3d_job_times = [p for p in other_job_times if "Class3D" in p[2]]
-    preproc_colours = {
-        "Import": "#1f77b4",
-        "MotionCorr": "#ff7f0e",
-        "CtfFind": "#2ca02c",
-        "AutoPick": "#9467bd",
-        "Extract": "#8c564b",
-        "Select": "#e377c2",
-        "crYOLO_AutoPick": "#d62728",
-        "Icebreaker_G": "#bcbd22",
-        "Icebreaker_F": "#17becf",
-    }
+    # preproc_colours = {
+    #    "Import": "#1f77b4",
+    #    "MotionCorr": "#ff7f0e",
+    #    "CtfFind": "#2ca02c",
+    #    "AutoPick": "#9467bd",
+    #    "Extract": "#8c564b",
+    #    "Select": "#e377c2",
+    #    "crYOLO_AutoPick": "#d62728",
+    #    "Icebreaker_G": "#bcbd22",
+    #    "Icebreaker_F": "#17becf",
+    # }
     starts = [p[0] for p in preproc_job_times[:-1]]
     ends = [p[0] for p in preproc_job_times[1:]]
     hover_names = [p[1].split("/")[0] for p in preproc_job_times[:-1]]
-    colours = [preproc_colours[p] for p in hover_names]
+    df = pd.DataFrame(preproc_job_times)
     timeline = px.timeline(
-        x_start=starts,
-        x_end=ends,
-        hover_name=hover_names,
-        color=colours,
-        labels=hover_names,
+        preproc_job_times,
+        x_start="start_time",
+        x_end="end_time",
+        hover_name="job",
+        color="job",
     )
     class2d_trace = px.timeline(
         x_start=[t[0] for t in class2d_job_times] + [t[0] for t in class3d_job_times],
@@ -91,9 +95,10 @@ def run() -> None:
         cumulative_time_spent["time"].append(ts)
         cumulative_time_spent["job"].append(h)
         cumulative_time_spent["colour"].append("#ff9000")
-    for t in other_time_spent:
+    for t, h in zip(other_time_spent, [p[2] for p in other_job_times]):
+        h = h.split("_batch")[0]
         cumulative_time_spent["time"].append(t)
-        cumulative_time_spent["job"].append(t[2].split("/")[0])
+        cumulative_time_spent["job"].append(h.split("/")[0])
         cumulative_time_spent["colour"].append("#2ec4b6")
     df = pd.DataFrame(cumulative_time_spent)
     cumulative_time = px.bar(df, x="job", y="time", color="colour")
