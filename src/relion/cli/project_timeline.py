b2d80a71f455ee780dcdf5418e569c5cc0d4c1e6
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-10-12 15:29:34 +0100

    use dataframes for everything in project_timeline

--- src/relion/cli/project_timeline.py
+++ src/relion/cli/project_timeline.py
@@ -46,37 +46,33 @@ def run() -> None:
     preproc_job_times["end_time"] = [
         end_times[t] for t in preproc_job_times["start_time"]
     ]
-    class2d_job_times = [p for p in other_job_times if "Class2D" in p[2]]
-    class3d_job_times = [p for p in other_job_times if "Class3D" in p[2]]
-    # preproc_colours = {
-    #    "Import": "#1f77b4",
-    #    "MotionCorr": "#ff7f0e",
-    #    "CtfFind": "#2ca02c",
-    #    "AutoPick": "#9467bd",
-    #    "Extract": "#8c564b",
-    #    "Select": "#e377c2",
-    #    "crYOLO_AutoPick": "#d62728",
-    #    "Icebreaker_G": "#bcbd22",
-    #    "Icebreaker_F": "#17becf",
-    # }
-    starts = [p[0] for p in preproc_job_times[:-1]]
-    ends = [p[0] for p in preproc_job_times[1:]]
-    hover_names = [p[1].split("/")[0] for p in preproc_job_times[:-1]]
+    preproc_job_times["total_time"] = [
+        datetime.timestamp(te) - datetime.timestamp(ts)
+        for te, ts in zip(
+            preproc_job_times["start_time"], preproc_job_times["end_time"]
+        )
+    ]
+
+    other_job_times["total_time"] = [
+        datetime.timestamp(te) - datetime.timestamp(ts)
+        for te, ts in zip(other_job_times["start_time"], other_job_times["end_time"])
+    ]
+
     df = pd.DataFrame(preproc_job_times)
+    df_other = pd.DataFrame(other_job_times)
     timeline = px.timeline(
-        preproc_job_times,
+        df,
         x_start="start_time",
         x_end="end_time",
         hover_name="job",
         color="job",
     )
     class2d_trace = px.timeline(
-        x_start=[t[0] for t in class2d_job_times] + [t[0] for t in class3d_job_times],
-        x_end=[t[1] for t in class2d_job_times] + [t[1] for t in class3d_job_times],
-        hover_name=["Class2D" for _ in class2d_job_times]
-        + ["Class3D" for _ in class2d_job_times],
-        color=["#ff7f0e" for _ in class2d_job_times]
-        + ["#8c564b" for _ in class2d_job_times],
+        df_other,
+        x_start="start_time",
+        x_end="end_time",
+        hover_name="job",
+        color="job",
     )
     timeline.write_html(
         pathlib.Path(args.out_dir) / "relion_project_preprocessing_timeline.html"
@@ -84,25 +80,10 @@ def run() -> None:
     class2d_trace.write_html(
         pathlib.Path(args.out_dir) / "relion_project_classification_timeline.html"
     )
-    time_spent = [
-        datetime.timestamp(te) - datetime.timestamp(ts) for ts, te in zip(starts, ends)
-    ]
-    other_time_spent = [
-        datetime.timestamp(t[1]) - datetime.timestamp(t[0]) for t in other_job_times
-    ]
-    cumulative_time_spent = {"time": [], "job": [], "colour": []}
 
-    for ts, h in zip(time_spent, hover_names):
-        cumulative_time_spent["time"].append(ts)
-        cumulative_time_spent["job"].append(h)
-        cumulative_time_spent["colour"].append("#ff9000")
-    for t, h in zip(other_time_spent, [p[2] for p in other_job_times]):
-        h = h.split("_batch")[0]
-        cumulative_time_spent["time"].append(t)
-        cumulative_time_spent["job"].append(h.split("/")[0])
-        cumulative_time_spent["colour"].append("#2ec4b6")
-    df = pd.DataFrame(cumulative_time_spent)
-    cumulative_time = px.bar(df, x="job", y="time", color="colour")
+    df_all = pd.merge(df, df_other)
+
+    cumulative_time = px.bar(df_all, x="job", y="total_time")
     cumulative_time.write_html(
         pathlib.Path(args.out_dir) / "cumulative_preprcoessing_job_time.html"
     )
