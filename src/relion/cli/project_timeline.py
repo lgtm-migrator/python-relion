08532f6eb9318a897c35414f0e4b98feb0e252bf
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-10-13 17:49:40 +0100

    moving everything to one html document
    in dire need of a refactor

--- src/relion/cli/project_timeline.py
+++ src/relion/cli/project_timeline.py
@@ -2,9 +2,11 @@ import argparse
 import pathlib
 from datetime import datetime
 
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from relion import Project
 
@@ -176,22 +178,190 @@ def run() -> None:
 
     df_all.sort_values("start_time")
 
-    cumulative_time = px.bar(
-        df_all,
-        x="job",
-        y="total_time",
-        color="schedule",
-        hover_data=["start_time", "end_time", "cluster_id", "num_mics", "total_time"],
-        labels={"total_time": "Total job time [s]"},
+    fig_times = make_subplots(
+        rows=1, cols=2, shared_yaxes=True, subplot_titles=("Total job time", "Run time")
     )
-    run_time = px.bar(
-        df_all,
-        x="job",
-        y="run_time",
-        color="schedule",
-        hover_data=["start_time", "end_time", "cluster_id", "num_mics", "total_time"],
-        labels={"run_time": "Cluster job time [s]"},
+
+    fig_times.add_trace(
+        go.Bar(
+            name="Preprocessing",
+            x=df_all[df_all.schedule.isin(["preprocess"])].job,
+            y=df_all[df_all.schedule.isin(["preprocess"])].total_time,
+            width=0.8,
+            customdata=np.transpose(
+                [
+                    df_all[df_all.schedule.isin(["preprocess"])].cluster_id,
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[df_all.schedule.isin(["preprocess"])].start_time
+                    ],
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[df_all.schedule.isin(["preprocess"])].end_time
+                    ],
+                ]
+            ),
+            hovertemplate="cluster ID: %{customdata[0]} <br>start time: %{customdata[1]|%Y/%m/%d %H:%M:%S} <br> end time: %{customdata[2]|%Y/%m/%d %H:%M:%S}",
+        ),
+        row=1,
+        col=1,
     )
+    fig_times.add_trace(
+        go.Bar(
+            name="Icebreaker group",
+            x=df_all[df_all.schedule.isin(["Icebreaker_group"])].job,
+            y=df_all[df_all.schedule.isin(["Icebreaker_group"])].total_time,
+            width=0.8,
+            customdata=np.transpose(
+                [
+                    df_all[df_all.schedule.isin(["Icebreaker_group"])].cluster_id,
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[
+                            df_all.schedule.isin(["Icebreaker_group"])
+                        ].start_time
+                    ],
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[
+                            df_all.schedule.isin(["Icebreaker_group"])
+                        ].end_time
+                    ],
+                ]
+            ),
+            hovertemplate="cluster ID: %{customdata[0]} <br>start time: %{customdata[1]|%Y/%m/%d %H:%M:%S} <br> end time: %{customdata[2]|%Y/%m/%d %H:%M:%S}",
+        ),
+        row=1,
+        col=1,
+    )
+    fig_times.add_trace(
+        go.Bar(
+            name="Class2D",
+            x=df_all[df_all.schedule.isin(["Class2D"])].job,
+            y=df_all[df_all.schedule.isin(["Class2D"])].total_time,
+            width=0.8,
+            customdata=np.transpose(
+                [
+                    df_all[df_all.schedule.isin(["Class2D"])].cluster_id,
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[df_all.schedule.isin(["Class2D"])].start_time
+                    ],
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[df_all.schedule.isin(["Class2D"])].end_time
+                    ],
+                ]
+            ),
+            hovertemplate="cluster ID: %{customdata[0]} <br>start time: %{customdata[1]|%Y/%m/%d %H:%M:%S} <br> end time: %{customdata[2]|%Y/%m/%d %H:%M:%S}",
+        ),
+        row=1,
+        col=1,
+    )
+    fig_times.add_trace(
+        go.Bar(
+            name="Initital model",
+            x=df_all[df_all.schedule.isin(["InitialModel"])].job,
+            y=df_all[df_all.schedule.isin(["InitialModel"])].total_time,
+            width=0.8,
+            customdata=np.transpose(
+                [
+                    df_all[df_all.schedule.isin(["InitialModel"])].cluster_id,
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[
+                            df_all.schedule.isin(["InitialModel"])
+                        ].start_time
+                    ],
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[df_all.schedule.isin(["InitialModel"])].end_time
+                    ],
+                ]
+            ),
+            hovertemplate="cluster ID: %{customdata[0]} <br>start time: %{customdata[1]|%Y/%m/%d %H:%M:%S} <br> end time: %{customdata[2]|%Y/%m/%d %H:%M:%S}",
+        ),
+        row=1,
+        col=1,
+    )
+    fig_times.add_trace(
+        go.Bar(
+            name="Class3D",
+            x=df_all[df_all.schedule.isin(["Class3D"])].job,
+            y=df_all[df_all.schedule.isin(["Class3D"])].total_time,
+            width=0.8,
+            customdata=np.transpose(
+                [
+                    df_all[df_all.schedule.isin(["Class3D"])].cluster_id,
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[df_all.schedule.isin(["Class3D"])].start_time
+                    ],
+                    [
+                        t.to_pydatetime()
+                        for t in df_all[df_all.schedule.isin(["Class3D"])].end_time
+                    ],
+                ]
+            ),
+            hovertemplate="cluster ID: %{customdata[0]} <br>start time: %{customdata[1]|%Y/%m/%d %H:%M:%S} <br> end time: %{customdata[2]|%Y/%m/%d %H:%M:%S}",
+        ),
+        row=1,
+        col=1,
+    )
+
+    fig_times.add_trace(
+        go.Bar(
+            name="Preprocessing",
+            x=df_all[df_all.schedule.isin(["preprocess"])].job,
+            y=df_all[df_all.schedule.isin(["preprocess"])].run_time,
+            width=0.8,
+        ),
+        row=1,
+        col=2,
+    )
+    fig_times.add_trace(
+        go.Bar(
+            name="Icebreaker group",
+            x=df_all[df_all.schedule.isin(["Icebreaker_group"])].job,
+            y=df_all[df_all.schedule.isin(["Icebreaker_group"])].run_time,
+            width=0.8,
+        ),
+        row=1,
+        col=2,
+    )
+    fig_times.add_trace(
+        go.Bar(
+            name="Class2D",
+            x=df_all[df_all.schedule.isin(["Class2D"])].job,
+            y=df_all[df_all.schedule.isin(["Class2D"])].run_time,
+            width=0.8,
+        ),
+        row=1,
+        col=2,
+    )
+    fig_times.add_trace(
+        go.Bar(
+            name="Initital model",
+            x=df_all[df_all.schedule.isin(["InitialModel"])].job,
+            y=df_all[df_all.schedule.isin(["InitialModel"])].run_time,
+            width=0.8,
+        ),
+        row=1,
+        col=2,
+    )
+    fig_times.add_trace(
+        go.Bar(
+            name="Class3D",
+            x=df_all[df_all.schedule.isin(["Class3D"])].job,
+            y=df_all[df_all.schedule.isin(["Class3D"])].run_time,
+            width=0.8,
+        ),
+        row=1,
+        col=2,
+    )
+
+    fig_times.update_yaxes(title_text="Time [s]", row=1, col=1)
+
     job_count = px.bar(
         df_all,
         x="job",
@@ -200,19 +370,165 @@ def run() -> None:
         labels={"job": "Job count"},
     )
 
-    cumulative_time.write_html(
-        pathlib.Path(args.out_dir) / "cumulative_preprcoessing_job_time.html"
-    )
-    run_time.write_html(
-        pathlib.Path(args.out_dir) / "cumulative_cluster_job_run_time.html"
-    )
     job_count.write_html(pathlib.Path(args.out_dir) / "job_counts.html")
 
+    jobs_gpu = [
+        j
+        for j in df_all.job.unique()
+        if j
+        not in ("Icebreaker_G", "Icebreaker_F", "Icebreaker_group", "Import", "Select")
+    ]
+    jobs_cpu = list(
+        {
+            j for j in df_all.job.unique() if j not in ("Import", "Select")
+        }.symmetric_difference(set(jobs_gpu))
+    )
+    summed_run_times_gpu = [
+        sum(p[1]["run_time"] for p in df_all.iterrows() if p[1]["job"] == j)
+        for j in jobs_gpu
+    ]
+    summed_queue_times_gpu = [
+        sum(p[1]["queue_time"] for p in df_all.iterrows() if p[1]["job"] == j)
+        for j in jobs_gpu
+    ]
+    summed_run_times_cpu = [
+        sum(p[1]["run_time"] for p in df_all.iterrows() if p[1]["job"] == j)
+        for j in jobs_cpu
+    ]
+    summed_queue_times_cpu = [
+        sum(p[1]["queue_time"] for p in df_all.iterrows() if p[1]["job"] == j)
+        for j in jobs_cpu
+    ]
     fig = go.Figure(
         data=[
-            go.Bar(name="Run time", x=df_all.job, y=df_all.run_time),
-            go.Bar(name="Queue time", x=df_all.job, y=df_all.queue_time),
-        ]
+            go.Bar(
+                name="Run time (GPU)",
+                x=jobs_gpu,
+                y=summed_run_times_gpu,
+                marker={"color": "#6883ba"},
+            ),
+            go.Bar(
+                name="Queue time (GPU)",
+                x=jobs_gpu,
+                y=summed_queue_times_gpu,
+                marker={"color": "#ff5666"},
+            ),
+            go.Bar(
+                name="Run time (CPU)",
+                x=jobs_cpu,
+                y=summed_run_times_cpu,
+                marker={"color": "#6883ba"},
+                marker_pattern_shape="x",
+            ),
+            go.Bar(
+                name="Queue time (CPU)",
+                x=jobs_cpu,
+                y=summed_queue_times_cpu,
+                marker={"color": "#ff5666"},
+                marker_pattern_shape="x",
+            ),
+        ],
     )
     fig.update_layout(barmode="group")
-    fig.write_html(pathlib.Path(args.out_dir) / "queue_times.html")
+    # fig.write_html(pathlib.Path(args.out_dir) / "queue_times.html")
+
+    jobs_gpu = [
+        j
+        for j in df.job.unique()
+        if j
+        not in (
+            "Icebreaker_G",
+            "Icebreaker_F",
+            "Icebreaker_group",
+            "Import",
+            "Select",
+            "Extract",
+        )
+    ]
+    summed_run_times_useful_gpu = [
+        sum(
+            p[1]["run_time"]
+            for p in df_all.iterrows()
+            if p[1]["job"] == j and p[1]["num_mics"]
+        )
+        for j in jobs_gpu
+    ]
+    summed_run_times_useless_gpu = [
+        sum(
+            p[1]["run_time"]
+            for p in df_all.iterrows()
+            if p[1]["job"] == j and p[1]["num_mics"] == 0
+        )
+        for j in jobs_gpu
+    ]
+    fig_run_use = go.Figure(
+        data=[
+            go.Bar(
+                name="Useful",
+                x=jobs_gpu,
+                y=summed_run_times_useful_gpu,
+                marker={"color": "#6883ba"},
+            ),
+            go.Bar(
+                name="Useless",
+                x=jobs_gpu,
+                y=summed_run_times_useless_gpu,
+                marker={"color": "#ff5666"},
+            ),
+        ],
+    )
+    fig_run_use.update_layout(barmode="group")
+    # fig.write_html(pathlib.Path(args.out_dir) / "run_times_useful_vs_useless.html")
+
+    jobs_gpu = [
+        j
+        for j in df.job.unique()
+        if j
+        not in (
+            "Icebreaker_G",
+            "Icebreaker_F",
+            "Icebreaker_group",
+            "Import",
+            "Select",
+            "Extract",
+        )
+    ]
+    summed_times_useful_gpu = [
+        sum(
+            p[1]["total_time"]
+            for p in df_all.iterrows()
+            if p[1]["job"] == j and p[1]["num_mics"]
+        )
+        for j in jobs_gpu
+    ]
+    summed_times_useless_gpu = [
+        sum(
+            p[1]["total_time"]
+            for p in df_all.iterrows()
+            if p[1]["job"] == j and p[1]["num_mics"] == 0
+        )
+        for j in jobs_gpu
+    ]
+    fig_total_use = go.Figure(
+        data=[
+            go.Bar(
+                name="Useful",
+                x=jobs_gpu,
+                y=summed_times_useful_gpu,
+                marker={"color": "#6883ba"},
+            ),
+            go.Bar(
+                name="Useless",
+                x=jobs_gpu,
+                y=summed_times_useless_gpu,
+                marker={"color": "#ff5666"},
+            ),
+        ],
+    )
+    fig_total_use.update_layout(barmode="group")
+    # fig.write_html(pathlib.Path(args.out_dir) / "total_times_useful_vs_useless.html")
+    with open(pathlib.Path(args.out_dir) / "cluster_stats.html", "w") as f:
+        f.write(fig_times.to_html(full_html=False, include_plotlyjs="cdn"))
+        f.write(fig.to_html(full_html=False, include_plotlyjs="cdn"))
+        f.write(fig_run_use.to_html(full_html=False, include_plotlyjs="cdn"))
+        f.write(fig_total_use.to_html(full_html=False, include_plotlyjs="cdn"))
